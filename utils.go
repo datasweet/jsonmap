e6fc7dc17e0580dc959340481bdf2475ce82f482
rvibrac <romain.vibrac@altanova.fr>
2019-03-13 14:51:21 +0100

    Add a way to escape a path
    * Add a way to put a '.' in the name a field. Put a \\ before it
    
    * Add tests

--- utils.go
+++ utils.go
@@ -5,14 +5,23 @@ import "strings"
 func createPath(path string) []string {
 	var keys []string
 	infos := strings.Split(path, ".")
+	var tmp strings.Builder
 	for _, k := range infos {
-		parts := strings.Split(k, "[")
+		if len(k) > 0 && k[len(k)-1] == '\\' {
+			tmp.WriteString(k[:len(k)-1])
+			tmp.WriteString(".")
+			continue
+		} else {
+			tmp.WriteString(k)
+		}
+		parts := strings.Split(tmp.String(), "[")
 		for _, p := range parts {
 			t := strings.TrimSpace(strings.TrimSuffix(p, "]"))
 			if len(t) > 0 {
 				keys = append(keys, t)
 			}
 		}
+		tmp.Reset()
 	}
 	return keys
 }
